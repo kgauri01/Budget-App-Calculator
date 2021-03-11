6d1109d4aad559c105503e9a59c966d8ab8b4e22
kgauri01 <kgauri00@fesb.hr>
2021-03-11 23:21:09 +0100

    Second commit

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-
+  budgetItems: BudgetItem[] = new Array<BudgetItem>();
+  totalBudget: number = 0;
   constructor() { }
-
+  
   ngOnInit() {
   }
 
+  addItem( newItem: BudgetItem){
+    this.budgetItems.push(newItem);
+    this.totalBudget +=newItem.amount;
+  }
+
+  deleteItem(item:BudgetItem) {
+    let index = this.budgetItems.indexOf(item);
+    this.budgetItems.splice(index,1);
+    this.totalBudget -=item.amount;
+  }
 }
